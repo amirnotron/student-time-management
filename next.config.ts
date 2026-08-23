047d42d5691bc6d289d0ea32192b796fcfc3f507
Notron <amirnotron@outlook.com>
2026-08-24 02:32:53 +0330

    fix: add step to stop PM2 app before deployment to release file locks

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   turbopack: {},
 
-  allowedDevOrigins: ["192.168.1.104", "http://192.168.1.104:3000", '10.210.74.213'],
+  allowedDevOrigins: ["192.168.1.104", "http://192.168.1.104:3000", '10.210.74.213' , ' 10.251.6.149'],
 };
 
 module.exports = nextConfig;
